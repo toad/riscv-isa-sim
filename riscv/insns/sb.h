c832a83b79a357f002a5df0c5446095560d347c1
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-17 11:32:39 +0100

    Clear tag on a normal write.

--- riscv/insns/sb.h
+++ riscv/insns/sb.h
@@ -3,3 +3,4 @@ reg_t address = RS1 + insn.s_imm();
 tag_t mem_tag = MMU.tag_read(address);
 STORE_TAG_CHECK(mem_tag, address);
 MMU.store_uint8(address, RS2);
+MMU.tag_write(address & ~7, 0);
--- riscv/insns/sd.h
+++ riscv/insns/sd.h
@@ -4,4 +4,5 @@ tag_t mem_tag = MMU.tag_read(address);
 STORE_TAG_CHECK(mem_tag, address);
 // store data
 MMU.store_uint64(address, RS2);
-
+// store tag
+MMU.tag_write(address, 0);
